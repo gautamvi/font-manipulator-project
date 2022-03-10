590b7bd7e5bd8dbcf3dead2cb55d2b235e481837
gautamvi <85584126+gautamvi@users.noreply.github.com>
2022-03-09 16:48:01 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-g
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,550);
+    canavs.position(560,150);
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
