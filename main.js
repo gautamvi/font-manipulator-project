df7b3c1e1b7c4f68b77855158d0ea6b4cfb34dd2
gautamvi <85584126+gautamvi@users.noreply.github.com>
2022-03-17 17:43:52 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
     canvas = createCanvas(550,550);
-    canavs.position(560,150);
+    canvas.position(560,150);
     poseNet = ml5.poseNet(video,modelLoaded);
     poseNet.on("pose",gotPoses);
 }
@@ -11,8 +15,19 @@ function modelLoaded(){
     console.log("PoseNet is initialized");
 }
 
-function gotPoses(){
+function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = "+leftWristX+" rightWristX = "+rightWristX+" difference = "+difference);
     }
+}
+
+function draw(){
+    background("#FFD700");
+    textSize(difference);
+    fill("#FFA500");
+    text("Gautam",40,200);
 }
